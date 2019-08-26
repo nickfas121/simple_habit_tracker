b0154d656a53d7b76ab05ebd165a39af53b69382
Raghu Betina <raghu@firstdraft.com>
2019-08-26 17:35:38 +0000

    Whitelist columns in Completion for ActiveAdmin

--- app/admin/completions.rb
+++ app/admin/completions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Completion do
+
+ permit_params :habit_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
