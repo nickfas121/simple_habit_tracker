1d8f37abce9c334939918d9679010fb7106598d0
Raghu Betina <raghu@firstdraft.com>
2019-08-26 17:35:14 +0000

    Direct association between users and completions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :completions,
+             :dependent => :destroy
+
   has_many   :habits,
              :dependent => :destroy
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:completions) }
+
     it { should have_many(:habits) }
 
     end
